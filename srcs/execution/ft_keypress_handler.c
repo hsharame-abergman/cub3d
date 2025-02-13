6ccd5543f0dd369baee8364f0185986f117ef8a3
abergman <abergman@student.42.fr>
2025-02-14 00:18:33 +0100

    chore(*): refactoring movement functions

--- srcs/execution/ft_keypress_handler.c
+++ srcs/execution/ft_keypress_handler.c
@@ -6,7 +6,7 @@
 /*   By: abergman <abergman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 23:20:27 by abergman          #+#    #+#             */
-/*   Updated: 2025/02/05 19:11:13 by abergman         ###   ########.fr       */
+/*   Updated: 2025/02/14 00:12:42 by abergman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,58 +14,62 @@
 
 static void	ft_move_forward(t_data *store)
 {
+	double new_x;
+	double new_y;
+
+	new_x = store->player->dir_x * PLAYERS_SPEED + 0.001;
+	new_y = store->player->dir_y * PLAYERS_SPEED + 0.001;
 	if (store->map->map_grid[(int)store->player->y][(int)(store->player->x
-		+ store->player->dir_x * PLAYERS_SPEED + 0.001)] == '0'
-		|| store->map->map_grid[(int)store->player->y][(int)(store->player->x
-			+ store->player->dir_x * PLAYERS_SPEED + 0.001)] == ' ')
-		store->player->x += store->player->dir_x * PLAYERS_SPEED + 0.001;
+		+ store->player->dir_x * PLAYERS_SPEED + 0.001)] == '0')
+		store->player->x += new_x;
 	if (store->map->map_grid[(int)(store->player->y + store->player->dir_y
-			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == '0'
-		|| store->map->map_grid[(int)(store->player->y + store->player->dir_y
-			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == ' ')
-		store->player->y += store->player->dir_y * PLAYERS_SPEED + 0.001;
+			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == '0')
+		store->player->y += new_y;
 }
 
 static void	ft_move_back(t_data *store)
 {
+	double new_x;
+	double new_y;
+
+	new_x = store->player->dir_x * PLAYERS_SPEED + 0.001;
+	new_y = store->player->dir_y * PLAYERS_SPEED + 0.001;
 	if (store->map->map_grid[(int)store->player->y][(int)(store->player->x
-		- store->player->dir_x * PLAYERS_SPEED + 0.001)] == '0'
-		|| store->map->map_grid[(int)store->player->y][(int)(store->player->x
-			- store->player->dir_x * PLAYERS_SPEED + 0.001)] == ' ')
-		store->player->x -= store->player->dir_x * PLAYERS_SPEED + 0.001;
+		- store->player->dir_x * PLAYERS_SPEED + 0.001)] == '0')
+		store->player->x -= new_x;
 	if (store->map->map_grid[(int)(store->player->y - store->player->dir_y
-			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == '0'
-		|| store->map->map_grid[(int)(store->player->y - store->player->dir_y
-			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == ' ')
-		store->player->y -= store->player->dir_y * PLAYERS_SPEED + 0.001;
+			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == '0')
+		store->player->y -= new_y;
 }
 
 static void	ft_move_left(t_data *store)
 {
+	double new_x;
+	double new_y;
+
+	new_x = store->player->vector_x * PLAYERS_SPEED + 0.001;
+	new_y = store->player->vector_y * PLAYERS_SPEED + 0.001;
 	if (store->map->map_grid[(int)store->player->y][(int)(store->player->x
-		- store->player->vector_x * PLAYERS_SPEED + 0.001)] == '0'
-		|| store->map->map_grid[(int)store->player->y][(int)(store->player->x
-			- store->player->vector_x * PLAYERS_SPEED + 0.001)] == ' ')
-		store->player->x -= store->player->vector_x * PLAYERS_SPEED + 0.001;
+		- store->player->vector_x * PLAYERS_SPEED + 0.001)] == '0')
+		store->player->x -= new_x;
 	if (store->map->map_grid[(int)(store->player->y - store->player->vector_y
-			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == '0'
-		|| store->map->map_grid[(int)(store->player->y - store->player->vector_y
-			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == ' ')
-		store->player->y -= store->player->vector_y * PLAYERS_SPEED + 0.001;
+			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == '0')
+		store->player->y -= new_y;
 }
 
 static void	ft_move_right(t_data *store)
 {
+	double new_x;
+	double new_y;
+
+	new_x = store->player->vector_x * PLAYERS_SPEED + 0.001;
+	new_y = store->player->vector_y * PLAYERS_SPEED + 0.001;
 	if (store->map->map_grid[(int)store->player->y][(int)(store->player->x
-		+ store->player->vector_x * PLAYERS_SPEED + 0.001)] == '0'
-		|| store->map->map_grid[(int)store->player->y][(int)(store->player->x
-			+ store->player->vector_x * PLAYERS_SPEED + 0.001)] == ' ')
-		store->player->x += store->player->vector_x * PLAYERS_SPEED + 0.001;
+		+ store->player->vector_x * PLAYERS_SPEED + 0.001)] == '0')
+		store->player->x += new_x;
 	if (store->map->map_grid[(int)(store->player->y + store->player->vector_y
-			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == '0'
-		|| store->map->map_grid[(int)(store->player->y + store->player->vector_y
-			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == ' ')
-		store->player->y += store->player->vector_y * PLAYERS_SPEED + 0.001;
+			* PLAYERS_SPEED + 0.001)][(int)store->player->x] == '0')
+		store->player->y += new_y;
 }
 
 int	ft_keypress_handler(int keycode, t_data *store)
